097beb011afa2a49b2d35f675d1736b2887ed296
Yishai <yishai@freightos.com>
2019-06-12 22:01:28 +0300

    implemented search

--- lib/pages/popular_screen.dart
+++ lib/pages/popular_screen.dart
@@ -1,52 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_movie_night/model/movie_model.dart';
 
-class PopularScreen extends StatefulWidget {
-  @override
-  _PopularScreenState createState() => _PopularScreenState();
-}
-
-class _PopularScreenState extends State<PopularScreen> {
+class PopularScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate:
-            new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        scrollDirection: Axis.vertical,
-        itemBuilder: (context, i) => new Card(
-            child: new Column(
-              children: <Widget>[
-                new Image(
-                  image: new NetworkImage(dummyData[i].getPosterImage()),
-                ),
-                new Text(
-                  dummyData[i].name,
-                  style: new TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 18.0),
-                )
-              ],
-            )));
-//    return ListView.builder(
-//        itemCount: dummyData.length,
-//        itemBuilder: (context, i) => new Column(
-//              children: <Widget>[
-//                new Divider(
-//                  height: 10.0,
-//                ),
-//                new ListTile(
-//                  leading: new CircleAvatar(
-//                    foregroundColor: Theme.of(context).primaryColor,
-//                    backgroundColor: Theme.of(context).accentColor,
-//                    backgroundImage:
-//                        new NetworkImage(dummyData[i].getPosterImage()),
-//                  ),
-//                  title: new Text(
-//                    dummyData[i].name,
-//                    style: new TextStyle(
-//                        fontSize: 18.0, fontWeight: FontWeight.bold),
-//                  ),
-//                )
-//              ],
-//            ));
+    return GridView.count(
+      crossAxisCount: 2,
+      padding: EdgeInsets.all(4.0),
+      childAspectRatio: 8.0 / 9.0,
+      children: dummyData
+          .map(
+            (item) => MovieTile(item),
+          )
+          .toList(),
+    );
+  }
+}
+
+class MovieTile extends StatelessWidget {
+  MovieTile(this.movie);
+
+  MovieModel movie;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // todo: implement movie details
+//        Navigator.push(
+//          context,
+//          MaterialPageRoute(
+//            builder: (context) => GridItemDetails(this.item),
+//          ),
+//        );
+        print("clicked on ${movie.name}");
+      },
+      child: Card(
+        elevation: 3.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 2 / 1.9,
+              child: Image.network(
+                movie.getPosterImage(),
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            new Padding(
+              padding: EdgeInsets.fromLTRB(4.0, 0.0, 4.0, 2.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 0.0),
+                    child: Center(
+                      child: Text(
+                        movie.name,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
