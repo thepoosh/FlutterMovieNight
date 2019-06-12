83d4affb6fbe96c8b95e91dc0fc394b87a10400d
Yishai <yishai@freightos.com>
2019-06-12 22:23:54 +0300

    initial search code

--- lib/pages/search_screen.dart
+++ lib/pages/search_screen.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_movie_night/model/movie_model.dart';
 
 class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: new Text(
-        "SEARCH",
-        style: new TextStyle(fontSize: 20.0),
-      ),
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            onChanged: (value) {},
+            controller: TextEditingController(),
+            decoration: InputDecoration(
+              labelText: "Search",
+              hintText: "Search",
+              prefixIcon: Icon(Icons.search),
+            ),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: dummyData.length,
+            itemBuilder: (context, i) {
+              final movie = dummyData[i];
+              return ListTile(
+                leading: Image.network(movie.getPosterImage()),
+                title: Text(movie.name),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
-}
 
+  Widget _buildListTile(MovieModel movie) {
+
+  }
+}
